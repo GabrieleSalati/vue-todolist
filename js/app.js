737145ef90dedd14ece4d6894bde1be0ca250f87
GabrieleSalati <gabriele.salati5@gmail.com>
2023-02-14 13:54:30 +0100

    added some graphic element and printed data from array

--- js/app.js
+++ js/app.js
@@ -2,7 +2,31 @@ const { createApp } = Vue;
 
 const app = createApp({
     data() {
-        return {};
+        return {
+            title: "To do list",
+            todo: [
+                {
+                    text: "Recuperare l'equipaggiamento.",
+                    done: true
+                },
+                {
+                    text: "Riunirsi con la squadra.",
+                    done: true
+                },
+                {
+                    text: "Assaltare la base.",
+                    done: false
+                },
+                {
+                    text: "Conquistare la bandiera.",
+                    done: false
+                },
+                {
+                    text: "Preparare le difese.",
+                    done: false
+                }
+            ]
+        };
     },
 
 });
